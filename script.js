586627ecf722048ddf9415f8f7cbe491662de3bd
Dionysis <dionysisterzios@gmail.com>
2022-09-11 16:44:48 +0300

    Create math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+console.log(add(4, 2));
+console.log(subtract(4, 2));
+console.log(multiply(4, 2));
+console.log(divide(4, 2));
