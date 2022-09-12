813de9e15fd38e67d636db5b67866b3e883e7344
Dionysis <dionysisterzios@gmail.com>
2022-09-12 15:14:50 +0300

    Refactor operators' functions

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
-
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
-
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
-}
+let add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 function operate(operator, firstNumber, secondNumber) {
     return operator(firstNumber, secondNumber);
 }
 
 console.log(operate(add, 4, 2));
+console.log(operate(subtract, 4, 2));
+console.log(operate(multiply, 4, 2));
+console.log(operate(divide, 4, 2));
