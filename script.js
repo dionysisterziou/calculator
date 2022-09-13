e4c05e135b9db2ca4effd8a5e718aba18e41b8c0
Dionysis <dionysisterzios@gmail.com>
2022-09-13 15:38:04 +0300

    Display values
    When 0, display the new value and when other number, concatenate them.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+// Operators
 let add = (firstNumber, secondNumber) => firstNumber + secondNumber;
 let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
 let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
@@ -5,4 +9,22 @@ let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 function operate(operator, firstNumber, secondNumber) {
     return operator(firstNumber, secondNumber);
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        let number = e.target.textContent;
+
+        if (checkIfZero(Number(display.textContent))) {
+            display.textContent = number;
+        } else {
+            display.textContent += number;
+        }
+    })
+})
+
+function checkIfZero(number) {
+    if (number === 0) {
+        return true;
+    }
 }
