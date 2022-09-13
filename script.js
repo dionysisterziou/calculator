945bf0917c0522ab7c6ed2e42dc3374204adbcdb
Dionysis <dionysisterzios@gmail.com>
2022-09-13 16:30:12 +0300

    Refactor script.js

--- script.js
+++ script.js
@@ -7,24 +7,26 @@ let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
 let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
 let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
-function operate(operator, firstNumber, secondNumber) {
-    return operator(firstNumber, secondNumber);
-}
-
-buttons.forEach(button => {
-    button.addEventListener('click', e => {
-        let number = e.target.textContent;
-
-        if (checkIfZero(Number(display.textContent))) {
-            display.textContent = number;
-        } else {
-            display.textContent += number;
-        }
-    })
-})
-
 function checkIfZero(number) {
     if (number === 0) {
         return true;
     }
+}
+
+function displayValue(button) {
+    let value = button.target.textContent;
+
+    if (checkIfZero(Number(display.textContent))) {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', displayValue);
+});
+
+function operate(operator, firstNumber, secondNumber) {
+    return operator(firstNumber, secondNumber);
 }
