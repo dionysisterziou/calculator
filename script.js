dcbd369eba8dbb5004f766392d6b669362ea10a9
Dionysis <dionysisterzios@gmail.com>
2022-09-17 22:39:46 +0300

    Refactor script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const buttons = document.querySelectorAll('button');
 const display = document.querySelector('#display');
 
 // Operators' functions
-let add = (firstNumber, secondNumber) => firstNumber + secondNumber;
-let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
-let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
-let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 let firstNumber = null;
 let secondNumber = null;
@@ -17,94 +17,17 @@ buttons.forEach(button => {
     button.addEventListener('click', displayValue);
 });
 
+// Other functions
 function displayValue(button) {
     let value = button.target.textContent;
     secondNumber = Number(display.textContent);
 
-    if (checkIfNumber(value)) {
-        if (defaultedDisplay) {
-            display.textContent = value;
-            defaultedDisplay = false;
-        } else {
-            if (previousValueWasNotNumber) {
-                display.textContent = value;
-                previousValueWasNotNumber = false;
-            } else {
-                display.textContent += value;
-            }
-        }
-    } else if (checkIfOperator(value)) {
-        if (operator !== null && !previousValueWasNotNumber) {
-            if (checkIfDividedByZero(operator, secondNumber)) {
-                display.textContent = NaN;
-            } else {
-                display.textContent = operate(operator, firstNumber, secondNumber);
-            }
-
-            operator = transformOperatorToFunction(value);
-        } else {
-            operator = transformOperatorToFunction(value);
-        }
-
-        firstNumber = Number(display.textContent);
-        previousValueWasNotNumber = true;
-    } else if (checkIfEqual(value)) {
-        if (checkIfDividedByZero(operator, secondNumber)) {
-            display.textContent = NaN;
-        } else {
-            display.textContent = operate(operator, firstNumber, secondNumber);
-        }
-
-        operator = null;
-        previousValueWasNotNumber = true;
-    } else if (checkIfAC(value)) {
-        display.textContent = 0;
-        operator = null;
-        previousValueWasNotNumber = null;
-        firstNumber = null;
-        secondNumber = null;
-        defaultedDisplay = true;
-    }
-
-    // It keeps the length of the displayed value at 9 digits
-    if (display.textContent.length > 9) {
-        display.textContent = display.textContent.substring(0, 9);
-    }
-
+    checkWhatTheValueIs(value)
+    checkTheDisplayedLength(); // Keeps it 9 digits
 }
 
 function operate(operator, firstNumber, secondNumber) {
     return operator(firstNumber, secondNumber);
-}
-
-function checkIfNumber(value) {
-    if (Number(value) >= 0 || Number(value) <= 9) {
-        return true;
-    }
-}
-
-function checkIfOperator(value) {
-    if (value === '+' || value === '-' || value === 'x' || value === '÷') {
-        return true;
-    }
-}
-
-function checkIfEqual(value) {
-    if (value === '=') {
-        return true;
-    }
-}
-
-function checkIfAC(value) {
-    if (value === 'AC') {
-        return true;
-    }
-}
-
-function checkIfDividedByZero(operator, secondNumber) {
-    if (operator === divide && secondNumber === 0) {
-        return true;
-    }
 }
 
 function transformOperatorToFunction(value) {
@@ -122,4 +45,81 @@ function transformOperatorToFunction(value) {
             value = divide;
             return value;
     }
+}
+
+// Check functions
+const checkIfNumber = value => Number(value) >= 0 || Number(value) <= 9;
+const checkIfEqual = value => value === '=';
+const checkIfAC = value => value === 'AC';
+const checkIfOperator = value => value === '+' || value === '-' ||
+    value === 'x' || value === '÷';
+
+function checkWhatTheValueIs(value) {
+    if (checkIfNumber(value)) {
+        applyIfNumber(value);
+    } else if (checkIfOperator(value)) {
+        applyIfOperator(value);
+    } else if (checkIfEqual(value)) {
+        applyIfEqual();
+    } else if (checkIfAC(value)) {
+        applyIfAC();
+    }
+}
+
+function checkIfDividedByZero() {
+    if (!isFinite(operate(operator, firstNumber, secondNumber))) {
+        display.textContent = NaN;
+    } else {
+        display.textContent = operate(operator, firstNumber, secondNumber)
+    }
+}
+
+function checkTheDisplayedLength() {
+    if (display.textContent.length > 9) {
+        display.textContent = display.textContent.substring(0, 9);
+    }
+}
+
+// Apply functions
+function applyIfNumber(value) {
+    if (defaultedDisplay) {
+        display.textContent = value;
+        defaultedDisplay = false;
+    } else {
+        if (previousValueWasNotNumber) {
+            display.textContent = value;
+            previousValueWasNotNumber = false;
+        } else {
+            display.textContent += value;
+        }
+    }
+}
+
+function applyIfOperator(value) {
+    if (operator !== null && !previousValueWasNotNumber) {
+        checkIfDividedByZero();
+
+        operator = transformOperatorToFunction(value);
+    } else {
+        operator = transformOperatorToFunction(value);
+    }
+
+    firstNumber = Number(display.textContent);
+    previousValueWasNotNumber = true;
+}
+
+function applyIfEqual() {
+    checkIfDividedByZero();
+
+    operator = null;
+    previousValueWasNotNumber = true;
+}
+
+function applyIfAC() {
+    display.textContent = 0;
+    operator = null;
+    previousValueWasNotNumber = null;
+    firstNumber = null;
+    secondNumber = null;
+    defaultedDisplay = true;
 }
